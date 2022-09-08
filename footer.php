bee11243c8e779bd361d7ab6ec8b831e1885ca81
Samrick <ricrdocardoso@gmail.com>
2022-09-08 19:41:08 -0300

    Delete footer.php

--- footer.php
+++
@@ -1,6 +0,0 @@
-        <div class="jumbotron text-center" style="margin-bottom:0"> <!-- jumbotron que é o rodapé-->
-            <p>Sistema Desenvolvimento para as Aulas de Programção Web do Professor Paulo Ricardo IFPR Telêmaco Borda</p>
-        </div>
-
-    </body>
-</html>
